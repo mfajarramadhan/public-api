a58bc9d3c30b65ab7eb35ccc4a0c180313547d6f
mfajarramadhan <mfajarramadhan04@gmail.com>
2024-09-24 13:08:20 +0700

    fix: fix responsive design

--- js/script.js
+++ js/script.js
@@ -30,48 +30,49 @@ function searchMovies(input) {
 
         $.each(movies, function (i, data) {
           $("#movie-list").append(`
-                  <div
-                    class="w-full sm:w-1/2 xl:w-1/3 mx-3 my-4 max-w-xs bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700"
-                  >
-                    <a href="#">
-                      <img class="w-full rounded-t-lg" src="${data.Poster}" alt="" />
-                    </a>
-                    <div class="p-5">
+              <div class="sm:w-1/2 xl:w-1/3 bg-white flex justify-center">
+          <div
+                      class="w-full my-4 max-w-xs bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700"
+                    >
                       <a href="#">
-                        <h5
-                          class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"
-                        >
-                          ${data.Title}
-                        </h5>
+                        <img class="w-full h-80 rounded-t-lg" src="${data.Poster}" alt="" />
                       </a>
-                      <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
-                        ${data.Year}
-                      </p>
-                      <a
-                        href="#"
-                        class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-                        id="static-modal-btn" data-id="${data.imdbID}"
-                      >
-                        See details
-                        <svg
-                          class="rtl:rotate-180 w-3.5 h-3.5 ms-2"
-                          aria-hidden="true"
-                          xmlns="http://www.w3.org/2000/svg"
-                          fill="none"
-                          viewBox="0 0 14 10"
+                      <div class="p-5">
+                        <a href="#">
+                          <h5
+                            class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"
+                          >
+                            ${data.Title}
+                          </h5>
+                        </a>
+                        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                          ${data.Year}
+                        </p>
+                        <a
+                          href="#"
+                          class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                          id="static-modal-btn" data-id="${data.imdbID}"
                         >
-                          <path
-                            stroke="currentColor"
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
-                            stroke-width="2"
-                            d="M1 5h12m0 0L9 1m4 4L9 9"
-                          />
-                        </svg>
-                      </a>
-                    </div>
-                  </div>
-                </div>
+                          See details
+                          <svg
+                            class="rtl:rotate-180 w-3.5 h-3.5 ms-2"
+                            aria-hidden="true"
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 14 10"
+                          >
+                            <path
+                              stroke="currentColor"
+                              stroke-linecap="round"
+                              stroke-linejoin="round"
+                              stroke-width="2"
+                              d="M1 5h12m0 0L9 1m4 4L9 9"
+                            />
+                          </svg>
+                        </a>
+                      </div>
+          </div>
+        </div>
             `);
         });
         $("#search-input2").val("");
